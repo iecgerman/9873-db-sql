c87e565afa6377d25389a39cef312400206895e8
iecgerman <iec_german@hotmail.com>
2024-07-02 02:13:50 -0600

    practica 24 terminada

--- practica24.sql
+++ practica24.sql
@@ -22,3 +22,63 @@ CREATE TABLE marcas (
     LOAD_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UPDATE_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
+
+SELECT * FROM marcas
+
+--PRIMERO INSERTAMOS LAS TABLAS QUE NO TIENEN LLAVES FORANEAS, PORQUE LUEGO MARCA ERROR AL TRATAR DE INSERTAR TABLAS CON LLAVES FORANEAS
+
+INSERT INTO marcas (marca_id, nombre)
+VALUES ('1', 'Dell')
+
+INSERT INTO marcas (marca_id, nombre)
+VALUES ('2', 'Apple');
+
+INSERT INTO marcas (marca_id, nombre)
+VALUES ('3', 'Samsung');
+
+SELECT * FROM productos
+
+INSERT INTO productos (producto_id, nombre, marca_id, precio)
+VALUES ('1', 'Laptop', '1', '1200.00');
+
+INSERT INTO productos (producto_id, nombre, marca_id, precio)
+VALUES ('2', 'Smartphone', '2', '800.00');
+
+INSERT INTO productos (producto_id, nombre, marca_id, precio)
+VALUES ('3', 'Tablet', '1', '400.00');
+
+--AHORA SI LO VISTO EN LA CLASE
+
+SELECT *
+FROM productos
+INNER JOIN marcas ON productos.marca_id = marcas.marca_id
+
+SELECT p.producto_id, p.nombre, m.marca_id, m.nombre
+FROM productos AS p
+INNER JOIN marcas AS m ON p.marca_id = m.marca_id
+
+SELECT p.producto_id, p.nombre, m.marca_id, m.nombre
+FROM productos AS p
+RIGHT JOIN marcas AS m ON p.marca_id = m.marca_id
+
+--FULL OUTER NO SE PUEDE EN MYSQL
+
+SELECT p.producto_id, p.nombre, m.marca_id, m.nombre
+FROM productos AS p
+FULL OUTER marcas AS m ON p.marca_id = m.marca_id 
+
+--ESTO ES UN FULL OUTER JOIN, LA UNION DE UN LEFT Y UN RIGHT
+
+SELECT p.producto_id, p.nombre, m.marca_id, m.nombre
+FROM productos AS p
+LEFT JOIN marcas AS m ON p.marca_id = m.marca_id
+UNION
+SELECT p.producto_id, p.nombre, m.marca_id, m.nombre
+FROM productos AS p
+RIGHT JOIN marcas AS m ON p.marca_id = m.marca_id
+
+-- CROSS JOIN
+
+SELECT p.producto_id, p.nombre, m.nombre
+FROM productos AS p
+CROSS JOIN marcas AS m
